4b15e6387f805604b6a021b85c2cb76480b89ea1
maxwelndiegu23456-pixel <maxwelndiegu23456@gmail.com>
2026-05-05 04:01:42 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//This changes your greeting based on the hour of the day 
+const hour = new Date().getHours();
+let greeting;
+
+if (hour < 12) {
+    greeting = "Good Morning! I'm";
+} else if (hour < 18) {
+    greeting = "Good Afternoon! I'm";
+} else {
+    greeting = "Good Evening! I'm";
+}
+
+const greetingElement = document.querySelector('.greeting-text');
+
+greetingElement.innerHTML = greeting;
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Designer", "Programmer", "Tech Creator", "Night Owl"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+//This will stop the animation after 10,00 milliseconds (10 seconds)
+setTimeout(() => {
+    typed.destroy(); //This removes the aninamtion
+    document.querySelector(".typing").innerHTML = "Developer" //this sets the final statement
+}, 20000);
